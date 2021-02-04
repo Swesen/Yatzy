8edbd8386a5b96aeb3dec342eefe0faa06d3333b
Linus Boréus <38762425+Swesen@users.noreply.github.com>
2021-02-04 12:29:31 +0100

    Started the structuring of the program.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,27 @@ namespace Yatzy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Conflict test");
+            // program entry ask players for the number of players
+
+            // ask eatch player for their name
+
+            // enter game loop
+
+            // print result and ask for a rematch with same players or new players
+        }
+
+        static int AskForNumberOfPlayers()
+        {
+            Console.Write("How many players: ");
+            string userInput = Console.ReadLine();
+
+            if (int.TryParse(userInput, out int amountOfPlayers))
+            {
+                return amountOfPlayers;
+            }
+
+            return 0; // error in player input handle in main
+
         }
     }
 }
